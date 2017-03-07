69f039f56ac161c163958cfa64b6b534c2bb57dd
Ilya Finkelshteyn <IlyaFinkelshteyn@users.noreply.github.com>
2017-03-07 15:17:16 -0800

    Update before-deploy.ps1

--- WebApplication3/before-deploy.ps1
+++ WebApplication3/before-deploy.ps1
@@ -1,6 +1,8 @@
 write-host "calling before-deploy.ps1"
 write-host $env:myVar
-if (!$env:applicationName) {$env:applicationName = ""}
+if ($env:applicationName -eq $null) {write-host "applicationName is null"; $env:applicationName = ""}
+if ($env:applicationName -ne $null) {write-host "applicationName is not null now"}
+
 #write-host "Getting env var..."
 #Get-ChildItem Env:
 
