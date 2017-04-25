d1cc46c5514cf42d1fa0f42d86c37e29b0e67c92
Ilya Finkelshteyn <IlyaFinkelshteyn@users.noreply.github.com>
2017-04-24 17:46:24 -0700

    [skip ci]

--- WebApplication3/before-deploy.ps1
+++ WebApplication3/before-deploy.ps1
@@ -1,9 +1,2 @@
 write-host "calling before-deploy.ps1"
-write-host "skeeping 10"
-sleep 10
-#write-host $env:myVar
-#if ($env:applicationName -eq $null) {write-host "applicationName is null"; $env:applicationName = ""; $applicationName = "" }
-#throw
-#write-host "Getting env var..."
-#Get-ChildItem Env:
-
+$env:SHOULD_CONTINUE = "123"
